ce9946ee7176dd4ddfc35935a16cd4946ce1dc7b
Abhimanyu Jain <abhimanyujain2k6@gmail.com>
2019-09-09 17:47:42 -0400

    Printing Library

--- education/education.py
+++ education/education.py
@@ -2,6 +2,6 @@ from .library import draw_library
 from .school import draw_school
 
 def draw_education():
-    draw_library()
+    draw_library("Mugar Library", 20, 20)
     draw_school()
     return
--- education/library.py
+++ education/library.py
@@ -1,3 +1,42 @@
-def draw_library():
-    print("library not found")
+def draw_horizontal_wall(length):
+	print("|", end = "")
+	for l in range(length):
+        	print("-", end = "")
+	print("|")
+
+	
+def draw_middle_section(width, length):
+	for w in range(width):
+		print("|", end = "")
+		for l in range(length):
+			print(" ", end = "")
+		print("|")
+
+def draw_library(name, length, width):
+  
+    """
+    Draws a library of a given length and width
+
+    args : 
+
+    name is the name of the library
+    length is the horizontal length of the library
+    width is the vertical length of the library
+
+    """
+    
+    # Sanitizing Parameters
+    if(length <= 0 or width <= 0):
+    	raise ValueError("Parameters should be greater than 0.")
+    	return
+
+    # Printing Library
+    print(name)
+    draw_horizontal_wall(length)
+    draw_middle_section(width, length)
+    draw_horizontal_wall(length)
+
     return
+	   
+
+
